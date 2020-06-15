a3c9227a99c0aa7773005906ad283d5e0774c915
Rômulo Ramos de Siqueira <romuloramosdesiqueira@MacBook-Pro-de-Romulo.local>
2020-06-15 00:04:01 -0300

    alterações radar

--- src/app/radar/radar.component.ts
+++ src/app/radar/radar.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./radar.component.scss']
 })
 export class RadarComponent implements OnInit {
+  public array = [0,1,2,3,4,5,6,7,8,9];
+
+  public planes = [
+    {
+      x: 3,
+      y: 3,
+      angle: 90
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  fixX(x) {
+    return (x * 60) + 299 - 15;
+  }
+
+  fixY(y) {
+    return (y * -60) + 299 - 15;
+  }
+
 
 }
