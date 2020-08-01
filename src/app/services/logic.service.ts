101628d57114716f57bf03cbf4248e7c9746bd4c
rogervieiraa <rogeramorinvieiraa@gmail.com>
2020-08-01 13:27:54 -0300

    add last function to logic

--- src/app/services/logic.service.ts
+++ src/app/services/logic.service.ts
@@ -101,13 +101,81 @@ export class LogicService {
 
   }
 
+  calculated_finish_point(plane, time) {
+    let x,y;
+  
+    let distance = time * plane.velocity
+    let degrees = plane.direction * Math.PI / 180
+    x = plane.x + distance * Math.sin(degrees)
+    y = plane.y + distance * Math.cos(degrees)
+  
+    return {"x": x, "y": y}
+  }
+  
+  calculate_f_x(p1, p2) {
+    let a = (p1.y - p2.y)/(p1.x - p2.x)
+    console.log(p1, p2)
+    let b = - 1 *(p1.x * a - p1.y)
+    return {"a": a, "b": b}
+  }
+
+  calculate_intersection_point(p1, p2, p3, p4) {
+    let f1, f2
+    f1 = this.calculate_f_x(p1, p2)
+    f2 = this.calculate_f_x(p3, p4)
+    /*
+    a * x + b = a * x + b
+    x (a1 - a2) = b2 - b1
+
+    */
+    let x = (f2.b - f1.b)/(f1.a - f2.a)
+    let y = f1.a * x + f1.b
+
+    return {"x": x, "y": y}
+  }
+
+  calculate_timming(p1, p2, velocity) {
+      /*
+        S = So + Vt 
+        S - So = Vt
+        Delta(S)/V = t
+      */
+    return this.distance_bettewen_two_points(p1.x, p1.y, p2.x, p2.y)/velocity
+  }
+  
   planes_in_collision_route(planes, minimum_time) {
+    let collision_planes = []
+    for(let i=0;i<planes.length;i++) {
+      let plane_i = planes[i]
+  
+      let pi1 = [plane_i.x, plane_i.y]
+      let pi2 = this.calculated_finish_point(plane_i, minimum_time)
+      console.log(pi1, pi2)
 
-    for(let i=0;i<minimum_time;i++) {
-      // move um, verifica se bateu na posicao que ele foi.
+      for(let j=i + 1;i<planes.length;j++) {
+        let  plane_j = planes[j]
+        let pj1 = [plane_j.x, plane_j.y]
+        let pj2 = this.calculated_finish_point(plane_j, minimum_time)
+        console.log(pj1, pj2)
+  
 
+        let intersection_point = this.calculate_intersection_point(pi1, pi2, pj1, pj2)
+        console.log(intersection_point)
+
+        let t1 = this.calculate_timming(pi1, intersection_point, plane_i.velocity)
+        let t2 = this.calculate_timming(pj1, intersection_point, plane_j.velocity)
+
+        console.log(t1, t2)
+        
+        if(t1 == t2) {
+          //Verificar se o negocio ta certo memo
+          collision_planes.push({"plane": [plane_i, plane_j], "distance": t1})
+        }
+
+      }
     }
 
+    return collision_planes
   }
 
   distance_bettewen_two_points(x1, y1, x2, y2) {
